e9eb8b4227bad7afc0e9c807e79e9d92ec160d07
Кротова Алёна <61360466+lol50@users.noreply.github.com>
2026-05-08 21:55:14 +0300

    add base constants

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import random, os, json, sys
 import time
+
+BASE_W, BASE_H = 1200, 600
+BASE_GROUND = BASE_H - 85
+BASE_SPEED = 28
+MIN_SPAWN_DELAY = 1800
+MAX_SPAWN_DELAY = 3200
